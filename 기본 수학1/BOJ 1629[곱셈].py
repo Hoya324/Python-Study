5ef436b077c775986194d48a880e1cc0479e553f
groomit324 <gangho324@naver.com>
2022-11-26 00:52:13 +0900

    p.s

--- 기본 수학1/BOJ 1629[곱셈].py
+++ 기본 수학1/BOJ 1629[곱셈].py
@@ -2,4 +2,15 @@ from sys import stdin
 
 a, b, c = map(int, stdin.readline().split())
 
-print(pow(a, b) % c)
+def divide(a, b):
+    if b == 1:
+        return a
+    if b % 2 == 0:
+        result = divide(a, b/2)
+        return result*result
+    else:
+        result = divide(a, (b-1)/2)
+        return result*result*a
+    
+
+print(divide(a, b) % c)
